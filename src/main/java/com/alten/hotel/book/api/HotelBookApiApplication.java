4a8320f7757f9a5f7d707d0eb1a865ce578eeb3d
Hugo Gois <gudshugo@gmail.com>
2021-11-16 16:13:12 -0300

    First commit with spring initializr template files

---
+++ src/main/java/com/alten/hotel/book/api/HotelBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.alten.hotel.book.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alten/hotel/book/api/HotelBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alten.hotel.book.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
