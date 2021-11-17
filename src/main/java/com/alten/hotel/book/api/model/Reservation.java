77fc190577465770b3de372a59300d11242f6acf
Hugo Gois <gudshugo@gmail.com>
2021-11-17 02:09:03 -0300

    Creating the API domain classes

--- src/main/java/com/alten/hotel/book/api/model/Reservation.java
+++ src/main/java/com/alten/hotel/book/api/model/Reservation.java
@@ -1,2 +1,42 @@
-package com.alten.hotel.book.api.model;public class Reservation {
+package com.alten.hotel.book.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+/**
+ * Class for persistence and reading that represents the reservation of a room.
+ * @author Hugo Gois
+ * @version 1.0
+ * @since 1.0
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@ApiModel("Class for persistence and reading that represents the reservation of a room.")
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private boolean isReserved = false;
+
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+    @ManyToOne
+    @JoinColumn
+    @JsonIgnore
+    private Room room;
+
 }
--- src/main/java/com/alten/hotel/book/api/model/Room.java
+++ src/main/java/com/alten/hotel/book/api/model/Room.java
@@ -1,2 +1,37 @@
-package com.alten.hotel.book.api.model;public class Room {
+package com.alten.hotel.book.api.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.annotations.ApiModel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+/**
+ * Class for persistence and reading tha represents a hotel room.
+ * @author Hugo Gois
+ * @version 1.0
+ * @since 1.0
+ */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@ApiModel("Class for persistence and reading tha represents a hotel room.")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
+    @JoinColumn(name = "room")
+    @JsonIgnore
+    private List<Reservation> reservation;
+
 }
